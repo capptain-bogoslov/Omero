9d2cc7c9a7829ea3ba93148e2cc8212f26fc1cc1
bogoslov <theo.bats1@gmail.com>
2022-11-13 13:39:25 +0200

    [A] Add colors

--- Omero/app/src/main/java/com/capptain/omero/ui/theme/Color.kt
+++ Omero/app/src/main/java/com/capptain/omero/ui/theme/Color.kt
@@ -9,3 +9,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundLight = Color(0xFFEFDFF8)
+val BackgroundDark = Color(0xFFA009F6)
+
+val PrimaryLight = Color(0xFFD05CE3)
+val SecondaryLight = Color(0xFF8F9BFF)
+val AccentLight = Color(0xFF9EFFFF)
+
+val PrimaryDark = Color(0xFF640080)
+val SecondaryDark = Color(0xFF0043CA)
+val AccentDark = Color(0xFF14CBA8)
--- Omero/app/src/main/java/com/capptain/omero/ui/theme/Theme.kt
+++ Omero/app/src/main/java/com/capptain/omero/ui/theme/Theme.kt
@@ -15,16 +15,19 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
+
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = BackgroundDark
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = BackgroundLight
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
