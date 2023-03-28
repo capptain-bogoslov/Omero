54e9dd23affff6f4b03c56ad619f7ff7e0940fb5
bogoslov <theo.bats1@gmail.com>
2023-03-28 22:37:40 +0300

    [A] Add UI and style for credentials

--- Omero/app/src/main/java/com/capptain/omero/AuthenticationActivity.kt
+++ Omero/app/src/main/java/com/capptain/omero/AuthenticationActivity.kt
@@ -6,21 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.capptain.omero.ui.theme.LinkBlueDark
 import com.capptain.omero.ui.theme.OmeroTheme
 import com.capptain.omero.ui.theme.robotoFamily
 
@@ -109,17 +120,81 @@ fun Logo() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CredentialsView() {
+    var email = remember { mutableStateOf(TextFieldValue()) }
+    var password = remember { mutableStateOf(TextFieldValue()) }
+
+
     Column {
+
         Text(
             text = stringResource(id = R.string.enter_app),
-            fontSize = 25.sp,
+            fontSize = 20.sp,
             fontFamily = robotoFamily,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 10.dp)
+                .padding(horizontal = 16.dp, vertical = 20.dp)
         )
+
+        //Username
+        TextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            placeholder = { Text(text = stringResource(id = R.string.email)) },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White,
+                placeholderColor = Color.LightGray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+        
+        Spacer(modifier = Modifier.height(10.dp))
+        
+        //Password
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            placeholder = { Text(text = stringResource(id = R.string.password)) },
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White,
+                placeholderColor = Color.LightGray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Forgot password
+        Box(modifier = Modifier.fillMaxWidth()) {
+            ClickableText(
+                text = AnnotatedString(stringResource(id = R.string.forgot_password)),
+                modifier = Modifier
+                    .align(Alignment.Center),
+                onClick = {},
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = robotoFamily,
+                    textDecoration = TextDecoration.Underline,
+                    color = LinkBlueDark
+                )
+            )
+        }
+//        Spacer(modifier = Modifier.fillMaxHeight())
+
+
+
     }
 }
 
--- Omero/app/src/main/java/com/capptain/omero/ui/theme/Color.kt
+++ Omero/app/src/main/java/com/capptain/omero/ui/theme/Color.kt
@@ -5,10 +5,12 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val LinkBlueLight = Color(0xFF8F9BFF)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val LinkBlueDark = Color(0xFF0043CA)
 
 val BackgroundLight = Color(0xFFEFDFF8)
 val BackgroundDark = Color(0xFFA009F6)
